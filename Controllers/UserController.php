db093a4c083191908cfa191693f7afa73f8f7d43
LuigiG34 <luigigandemer6@gmail.com>
2022-07-19 19:37:05 +0200

    User Controller

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -3,14 +3,30 @@
 namespace App\Controllers;
 
 use App\Models\UserManager;
-use PDO;
 
-class UserController
+class UserController extends GlobalController
 {
     private $UserManager;
 
     public function __construct()
     {
         $this->UserManager = new UserManager;
+    }
+
+    public function signUpUser()
+    {
+        if(isset($_POST['submit'])){
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $city = $_POST['city'];
+            $zipcode = $_POST['zipcode'];
+            $birthdate = $_POST['birthdate'];
+            $status = $_POST['status'];
+            if(isset($_POST['job'])){
+                $job = $_POST['job'];
+            }
+        }
     }
 }
