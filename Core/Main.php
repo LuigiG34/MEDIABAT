2927ea666607fb98bbf9788a72598bf54d8ddba5
LuigiG34 <luigigandemer6@gmail.com>
2022-07-19 19:37:31 +0200

    Router

--- Core/Main.php
+++ Core/Main.php
@@ -3,7 +3,6 @@
 namespace App\Core;
 
 use App\Controllers\ApiController;
-
 /**
  * Router Class
  */
@@ -18,17 +17,40 @@ class Main
     {
         // if there is nothing in url then display 404 ERROR
         if (!isset($_GET['page'])) {
-            http_response_code(404);
-            require "public/404.php";
+            require URL."public/Views/main/index.php";
         } else {
             // get $url and store each param in an array
             $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL);
             // If url = case
             switch ($url[0]) {
+                case 'accueil':
+                    require URL."public/Views/main/index.php";
+                    break;
+
+                case 'connexion':
+                    require URL."public/Views/auth/connexion.php";
+                    break;
+                
+                case 'inscription':
+                    require URL."public/Views/auth/inscription.php";
+                    break;
+
+                case 'contact':
+                    require URL."public/Views/contact/contact.php";
+                    break;
+                
+                case 'profil':
+                    if(isset($url[1]) && $url[1] === "modif"){
+                        var_dump($url);
+                        require URL."public/Views/users/modifier-profil.php";
+                    }else{
+                        require URL."public/Views/users/profil.php";
+                    }
+                    break;
                 default:
                     // If none of the above than display 404 ERROR
                     http_response_code(404);
-                    require "../public/views/error/404.php";
+                    require URL."public/views/error/404.php";
                     break;
             }
         }
